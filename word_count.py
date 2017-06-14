5b91386bbc3704e9c3aac732fca7e5f4e25eee04
joecatania <joecatania0@gmail.com>
2017-06-13 21:18:00 -0400

    version 1

--- word_count.py
+++ word_count.py
@@ -10,26 +10,33 @@ def word_count(file_name):
     f1 = open(file_name, 'r')
 
     l1 = []
-    word_count = 0
-    line_count = 0
+    word_num = 0
+    line_num = 0
 
     for line in f1:
-        line_count = line_count + 1
+
+        if len(line.strip()) != 0:
+            line_num = line_num + 1
+
         l1 = line.split()
-        word_count = len(l1) + word_count
+        word_num = len(l1) + word_num
 
-    print 'The words count: ', word_count
-    print 'the lines count: ', line_count
+    f1.close()
 
-    return 0
-
-    f1.close
+    return word_num, line_num
 
 
 def main():
-    print 'command line args: ', sys.argv[1]
+    if len(sys.argv) < 2:
+        print 'Usage: python word_count.py <file_name>'
+        exit(1)
+    else:
+        file_name = sys.argv[1]
 
-    result = word_count(sys.argv[1])
+    word_num, line_num = word_count(file_name)
+
+    print 'The words count: ', word_num
+    print 'the lines count: ', line_num
 
 
 if __name__ == '__main__':
